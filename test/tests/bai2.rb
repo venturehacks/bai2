05b60e0b431eb3f45d6c0978f26377ae898a4dc0
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-12-30 14:06:17 -0800

    tests

--- test/tests/bai2.rb
+++ test/tests/bai2.rb
@@ -13,4 +13,47 @@ class Bai2Test < Minitest::Test
     assert_kind_of(Bai2::BaiFile, @daily)
     assert_kind_of(Bai2::BaiFile, @eod)
   end
+
+  def test_groups
+    [@daily, @eod].each do |file|
+      assert_kind_of(Array, file.groups)
+      assert_equal(1, file.groups.count)
+      group = file.groups.first
+      assert_kind_of(Bai2::BaiFile::Group, group)
+      assert_equal('121140399', group.originator)
+    end
+    assert_equal('9999999999', @daily.groups[0].destination)
+    assert_equal('3333333333', @eod.groups[0].destination)
+  end
+
+  def test_accounts
+    [@daily, @eod].each do |file|
+      accounts = @daily.groups.first.accounts
+      assert_kind_of(Array, accounts)
+      assert_equal(1, accounts.count)
+      assert_kind_of(Bai2::BaiFile::Account, accounts.first)
+    end
+  end
+
+  def test_transactions
+    all_txs = [@daily, @eod].flat_map(&:groups).flat_map(&:accounts).flat_map(&:transactions)
+    assert_equal(2, all_txs.count)
+    all_txs.each do |tx|
+      assert_kind_of(Bai2::BaiFile::Transaction, tx)
+    end
+    first, second = all_txs
+    assert_equal(first.type, {
+      code: 174,
+      transaction: :credit,
+      scope: :detail,
+      description: 'Other Deposit'
+    })
+    assert_equal(second.type, {
+      code: 195,
+      transaction: :credit,
+      scope: :detail,
+      description: 'Incoming Money Transfer'
+    })
+  end
+
 end
---
+++ test/tests/parsing.rb
@@ -0,0 +1,9 @@
+require File.expand_path('../../autorun.rb', __FILE__)
+
+require 'bai2'
+
+class Bai2ParsingTest < Minitest::Test
+
+  # TODO: implement this, we might want to test the parsing internals, not just
+  # the result... in case of problems it might help debug
+end
